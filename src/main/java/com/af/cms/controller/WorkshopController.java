eb5c314aadbe799ba1086a0ec0954acd19f72a40
janith-ch <it19043388@my.sliit.lk>
2021-06-29 19:42:16 +0530

    conferance chnages

--- src/main/java/com/af/cms/controller/WorkshopController.java
+++ src/main/java/com/af/cms/controller/WorkshopController.java
@@ -8,14 +8,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.af.cms.model.Workshop;
@@ -26,6 +19,7 @@ import com.af.cms.respone.UploadFileResponse;
 
 
 @RestController
+@CrossOrigin
 public class WorkshopController {
 	
 	@Autowired
